e385fbb796afa66fd0e4e41ebd088790b8e815d1
Simba <nyakambs@uni.coventry.ac.uk>
2022-03-14 09:07:05 +0000

    GUI form

---
+++ src/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import javax.swing.*;
+
+public class Client {
+    private JTabbedPane Wines;
+    private JPanel panel1;
+    private JButton connectToServerButton;
+    private JButton editButton;
+    private JButton printButton;
+    private JButton deleteButton;
+    private JButton addButton;
+    private JButton getTableButton;
+    private JTable Winestable1;
+    private JButton GetTableCustomers;
+    private JButton DeleteCustomers;
+    private JButton EditCustomers;
+    private JButton PrintCustomers;
+    private JTable Customertable;
+    private JTable Reviewstable;
+    private JButton GetTablleReviews;
+    private JButton AddReviews;
+    private JButton EditReviews;
+    private JButton DeleteReviews;
+    private JButton PrintReviews;
+}
