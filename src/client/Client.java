c5511aa2a828cd8591f44a69f263f7704bf89b11
Simba <nyakambs@uni.coventry.ac.uk>
2022-03-14 09:31:10 +0000

    GUI form with textfields

--- src/client/Client.java
+++ src/client/Client.java
@@ -23,4 +23,30 @@ public class Client {
     private JButton EditReviews;
     private JButton DeleteReviews;
     private JButton PrintReviews;
+    private JTextField PointsTextfield;
+    private JTextField WineIdTextfield;
+    private JTextField CountryTextfield;
+    private JTextField DescriptionTextfield;
+    private JTextField DesignationTextfield;
+    private JTextField PriceTextfield;
+    private JTextField ProvinceTextfield;
+    private JTextField Region1Textfield;
+    private JTextField CustomerIdTextfield;
+    private JTextField FirstnameTextfield;
+    private JTextField LastnameTextfield;
+    private JTextField AddressTextfield;
+    private JTextField CityTextfield;
+    private JTextField CountryCustomersTextfield;
+    private JTextField PostalTextfield;
+    private JTextField Phone1Textfield;
+    private JTextField Phone2Textfield;
+    private JTextField EmailTextfield;
+    private JTextField ReviewIdTextfield;
+    private JTextField CustomerIDReviewsTextfield;
+    private JTextField WineIDTexfield;
+    private JTextField CustomerDescripTextfield;
+    private JTextField CustomerRatingeTextfield;
+    private JTextField DateAddedTextfield;
+
+
 }
